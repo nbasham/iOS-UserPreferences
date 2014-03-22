24b4b08d25e71ff22f0ed0fb0f2dd7942fefd2f9
nbasham <nbasham@yahoo.com>
2014-03-21 21:58:17 -0700

    Initial entry.

---
+++ iOS-UserPreferences/UserPreferences.h
@@ -0,0 +1,26 @@
+//
+//  UserPreferences.h
+//  iOS-UserPreferences
+//
+//  Created by Norman Basham iMac on 3/21/14.
+//  Copyright (c) 2014 isolace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserPreferences : NSObject
+
++(NSString*)getStringWithKey:(NSString*)key ;
++(NSString*)getStringWithKey:(NSString*)key withDefault:(NSString*)d;
++(void)setString:(NSString*)v withKey:(NSString*)key;
++(BOOL)getBoolWithKey:(NSString*)key;
++(BOOL)getBoolWithKey:(NSString*)key withDefault:(BOOL)d;
++(void)setBool:(BOOL)v withKey:(NSString*)key;
++(int)getIntWithKey:(NSString*)key;
++(int)getIntWithKey:(NSString*)key withDefault:(int)d;
++(void)setInt:(int)v withKey:(NSString*)key;
++(BOOL)keyExists:(NSString*)key;
++(BOOL)keyUndefined:(NSString*)key;
++(BOOL)isKeyUndefinedThenDefine:(NSString*)what;
+
+@end
